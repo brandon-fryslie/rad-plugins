9f88471b3ecb068e7468ab23ec7eaff846fbe2bb
brandon.fryslie <brandon.fryslie@workday.com>
2022-01-21 10:44:47 -0700

    homebrew plugin: Make it work w/ both Apple Silicon and Intel mac

--- homebrew/homebrew.plugin.zsh
+++ homebrew/homebrew.plugin.zsh
@@ -13,5 +13,13 @@ if [[ $(uname) == 'Darwin' ]]; then
   if ! type brew &>/dev/null; then
     rad-install-homebrew
   fi
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  if [[ -f /opt/homebrew/bin/brew ]]; then
+    # Apple Silicon
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [[ -f /usr/local/bin/brew ]]; then
+    # Intel Mac
+    eval "$(/usr/local/bin/brew shellenv)"
+  else
+    rad-red "rad-plugin homebrew: Could not load homebrew shell environment"
+  fi
 fi
