54c2fd33c165e719d97b17469a9510742f4e8564
Brandon Fryslie <530235+brandon-fryslie@users.noreply.github.com>
2025-05-25 01:10:55 -0600

    Setup direnv, pyenv, pipenv, and pipx in python plugin.  That should about cover the basics

--- python/python.plugin.zsh
+++ python/python.plugin.zsh
@@ -1,25 +1,49 @@
 #### python - opinionated and functional
 
-# TODO:
-# setup: pipenv, pyenv, direnv
 
-
-# direnv
-_direnv_hook() {
-  trap -- '' SIGINT
-  eval "$("/opt/homebrew/bin/direnv" export zsh)"
-  trap - SIGINT
-}
-typeset -ag precmd_functions
-if (( ! ${precmd_functions[(I)_direnv_hook]} )); then
-  precmd_functions=(_direnv_hook $precmd_functions)
-fi
-typeset -ag chpwd_functions
-if (( ! ${chpwd_functions[(I)_direnv_hook]} )); then
-  chpwd_functions=(_direnv_hook $chpwd_functions)
+############### direnv ###############
+#####################################
+if command -v direnv &> /dev/null; then
+  eval "$(/opt/homebrew/bin/direnv hook zsh)"
+else
+  rad-yellow "rad-python plugin: direnv is not installed.  consider installing it"
 fi
 # /direnv
 
+
+############### pyenv ###############
+#####################################
+# Only proceed if pyenv exists
+if command -v pyenv >/dev/null; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+
+  # Ensure pyenv is initialized by checking if `pyenv global` works
+  if ! command -v pyenv >/dev/null || [[ -z "$(pyenv root)" ]]; then
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+  fi
+
+  # Initialize pyenv-virtualenv only if available
+  if command -v pyenv-virtualenv-init >/dev/null; then
+    eval "$(pyenv virtualenv-init -)"
+  fi
+fi
+
+
+############### pipenv ###############
+#####################################
+# Set PIPENV_SHELL to zsh to ensure pipenv uses zsh
+export PIPENV_SHELL=zsh
+
+# pipenv completions
+if command -v pipenv &> /dev/null; then
+  eval "$(_PIPENV_COMPLETE=zsh_source pipenv)"
+fi
+
+
+############### pipx ###############
+#####################################
 # load pipx completions
 if command -v pipx &> /dev/null; then
   eval "$(register-python-argcomplete pipx)"
