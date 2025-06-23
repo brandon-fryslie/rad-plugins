90d31cdc9b0a153f8787849a2d0d31da64556abe
Brandon Fryslie <530235+brandon-fryslie@users.noreply.github.com>
2025-06-23 14:54:51 -0600

    homebrew plugin: Disable automatic homebrew install (it should already be installed anyway) (#5)

--- homebrew/homebrew.plugin.zsh
+++ homebrew/homebrew.plugin.zsh
@@ -1,18 +1,9 @@
 #### Plugin for Homebrew
 
-### macOS only
-### adds /usr/local/bin to your $PATH
-### Installs Homebrew if not already installed
-
-rad-install-homebrew() {
-  rad-red "Installing Homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-}
+### macOS only currently
+### Configures homebrew $PATH
 
 if [[ $(uname) == 'Darwin' ]]; then
-  if ! type brew &>/dev/null; then
-    rad-install-homebrew
-  fi
   if [[ -f /opt/homebrew/bin/brew ]]; then
     # Apple Silicon
     eval "$(/opt/homebrew/bin/brew shellenv)"
