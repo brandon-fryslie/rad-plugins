8e22696c5999fdf003aefc1ffe0f97f10edf287b
Brandon Fryslie <530235+brandon-fryslie@users.noreply.github.com>
2025-12-19 08:42:46 -0700

    wip

--- claude-code/claude-code.plugin.zsh
+++ claude-code/claude-code.plugin.zsh
@@ -1,2 +1,3 @@
 
 alias clod="SHELL=bash claude --dangerously-skip-permissions"
+alias zlod="ANTHROPIC_BASE_URL=https://api.z.ai/api/anthropic API_TIMEOUT_MS=3000000 ANTHROPIC_AUTH_TOKEN=${ANTHROPIC_AUTH_TOKEN:-MUST_SET_ANTHROPIC_TOKEN} clod"
